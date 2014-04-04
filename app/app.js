339f838e6f05038e777c9f24099a7a1bc323c6c8
Theodore Mathioudakis <tmathioudakis@gmail.com>
2014-04-04 17:18:26 +0300

    UI fixes

--- app/app.js
+++ app/app.js
@@ -91,6 +91,10 @@ listing.config(['$routeProvider',
 				templateUrl: 'templates/search.html',
 				controller: 'listingController'
 			}).
+			when('/home', {
+				templateUrl: 'templates/main.html',
+				controller: 'mainController'
+			}).
 			when('/', {
 				templateUrl: 'templates/main.html',
 				controller: 'mainController'
